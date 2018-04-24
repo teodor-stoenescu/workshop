e29408f36a965d67463504b2a4d10a102faabb92
Teodor Stoenescu <teodor-stoenescu@users.noreply.github.com>
2018-04-24 11:58:35 +0300

    Added WiFiConfig.h

---
+++ WiFiConfig.h
@@ -0,0 +1,16 @@
+#ifndef _WIFI_CONFIG_H_
+#define _WIFI_CONFIG_H_
+
+/* Place your WiFi SSID here */
+#define WIFI_SSID "<wifi_ssid>"
+
+/* Place your WiFi password here */
+#define WIFI_PASSWORD "<wifi_pass>"
+
+/* Put the MAX/MSP host port here. Use commas to separate IP values instead of dots */
+#define WIFI_MAX_HOST 192,168,1,100
+
+/* Put the UDP port where MAX/MSP is listening for OSC messages */
+#define WIFI_MAX_PORT 8888
+
+#endif
